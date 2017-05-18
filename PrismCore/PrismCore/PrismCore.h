36433c2794785efd91650feda071b9cbb325370d
Nanang Rafsanjani <jukiginanjar1989@gmail.com>
2017-05-18 11:26:51 +0700

    initial project

---
+++ PrismCore/PrismCore/PrismCore.h
@@ -0,0 +1,19 @@
+//
+//  PrismCore.h
+//  PrismCore
+//
+//  Created by Nanang Rafsanjani on 5/18/17.
+//  Copyright © 2017 Midtrans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrismCore.
+FOUNDATION_EXPORT double PrismCoreVersionNumber;
+
+//! Project version string for PrismCore.
+FOUNDATION_EXPORT const unsigned char PrismCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrismCore/PublicHeader.h>
+
+
---
+++ PrismUI/PrismUI/PrismUI.h
@@ -0,0 +1,19 @@
+//
+//  PrismUI.h
+//  PrismUI
+//
+//  Created by Nanang Rafsanjani on 5/18/17.
+//  Copyright © 2017 Midtrans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrismUI.
+FOUNDATION_EXPORT double PrismUIVersionNumber;
+
+//! Project version string for PrismUI.
+FOUNDATION_EXPORT const unsigned char PrismUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrismUI/PublicHeader.h>
+
+
